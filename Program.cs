58ff81b8333d7152b03c1ff8e69691a6d553a412
unknown <abhisheksrivastava9719@gmail.com>
2022-06-17 14:51:20 +0530

    Fibonacci series till nth term

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿Console.WriteLine("Welcome to part one of the assignment of day six");
+﻿Console.WriteLine("Enter how many fibonacci numbers you want");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 0;
+int b = 1;
+Console.WriteLine(a);
+Console.WriteLine(b);
+for (int x = 1; x <= n - 2; x++)
+{
+    int sum = a + b;
+    a = b;
+    b = sum;
+    Console.WriteLine(sum);
+}
 
